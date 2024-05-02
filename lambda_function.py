b0d57ae25e67ef7422d0930bf2170bfcb86a532b
Rakesh <happyending2012to16@gmail.com>
2024-05-02 22:07:03 +0530

    commit

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 #
 def lambda_handler(event, context):
     # TODO implement
-    print('another print statement')
+    print('another print statement ')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
