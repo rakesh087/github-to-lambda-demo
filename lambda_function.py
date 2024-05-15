014a7aec78d26eff6eb2a49442522d1cc11fe087
Rakesh <happyending2012to16@gmail.com>
2024-05-15 07:46:19 +0530

    print('done') added in lambda file in main branch

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import json
 def lambda_handler(event, context):
     # TODO implement
     print('another print statement again deleted')
+    print('done')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! fellow mate')
