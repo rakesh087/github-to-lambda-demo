19beff173b954947e068c9ba325fbba31d555ed1
rakesh087 <50709094+rakesh087@users.noreply.github.com>
2024-08-25 12:33:55 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,7 @@ def lambda_handler(event, context):
     print('another print statement again deleted')
     print('done')
     print('lambda modified on Aug-24')
+    print('it's again')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! fellow mate')
