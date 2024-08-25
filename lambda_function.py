052726fbc9d80e0ef9d0f678cc406077afec1678
rakesh087 <50709094+rakesh087@users.noreply.github.com>
2024-08-25 12:49:00 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     print('another print statement again deleted')
     print('done')
-    print('lambda modified on Aug-24')
     print('it's again')
     return {
         'statusCode': 200,
