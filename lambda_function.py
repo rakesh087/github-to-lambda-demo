5acda8014e250aa4a75d3191f2fca49c17733458
Rakesh <happyending2012to16@gmail.com>
2024-08-26 10:53:09 +0530

    branch_created

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # TODO implement
     print('another print statement again deleted')
     print('done')
-    print('it's again')
+    print('its again')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! fellow mate')
